4c7988ceb4a650ca3379a3602299a46e92ea05e5
mayur rathod <mayurrathod.7667@gmail.com>
2026-04-19 12:38:47 +0530

    adding code for pod info

--- src/main/java/com/example/hello/world/Application.java
+++ src/main/java/com/example/hello/world/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +15,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World Application Mayur!";
-	}
+	@GetMapping("/pod")
+	public String hello() throws UnknownHostException {
+		String hostname = InetAddress.getLocalHost().getHostName();
+		return "Response from Pod: " + hostname;
+		}
 }
